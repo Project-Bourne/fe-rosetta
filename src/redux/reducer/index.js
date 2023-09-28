2f1b63fc12806fa4e7dcf8e552510fd0dca3631c
chisom chima <chimachisom360@gmail.com>
2023-09-28 15:20:43 +0100

    fixes

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import TranslateSlice from './translateSlice';
 import TabSlice from './tabSlice'
+import authSlice from './authReducer'
 
 const rootReducer = combineReducers({ 
     translate: TranslateSlice,
-    tab: TabSlice
+    tab: TabSlice,
+    auth: authSlice
 });
                             
 export default rootReducer;
