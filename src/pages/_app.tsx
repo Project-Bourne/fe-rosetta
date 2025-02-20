2ec54a580a08d4e3b7bf42f01d4f9a8cc50ee32d
code-sensei <tsowababangida@gmail.com>
2025-02-20 17:44:37 +0100

    fix: object.hasOwn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { store, persistor } from "../redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import '@/styles/globals.css';
+import '../polyfills'; // Import the polyfill here
 
 function App({ Component, pageProps, ...appProps }) {
   const isLayoutNeeded = appProps.router.pathname.includes("/auth");
